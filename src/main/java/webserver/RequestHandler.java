a91b01dea130e35b09cc016fc07f99dc28ec2c4a
YANG <YANG@DESKTOP-2J98TND>
2019-03-04 23:00:51 +0900

    요구사항 1 - index.html 응답하기 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,15 +27,33 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	String path = getPath(in);
+        	
+        	// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = getContent(path);
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
+
+	private String getPath(InputStream in) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+		String clientMessage = bufferedReader.readLine();
+		String[] messages = clientMessage.split(" ");
+		return messages[1];
+	}
+	
+	private byte[] getContent(String path) throws IOException {
+		byte[] content = "Hello World".getBytes();
+    	if(path.equals("/index.html")) {
+    		content = Files.readAllBytes(new File("./webapp" + path).toPath());
+    	}
+		
+		return content;
+	}
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
